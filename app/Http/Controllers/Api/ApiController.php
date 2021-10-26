927865dbabe96606caf2fefa68618be0581d9d52
Janagiramanm <janagiraman@netiapps.com>
2021-10-26 10:45:39 +0530

    debug

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -93,7 +93,7 @@ class ApiController extends Controller
 
     public function userLocation(Request $request){
 
-           $userLocations = TrackLocations::get();
+           $userLocations = TrackLocations::where('date','=','2021-10-25')->get();
          
            return [
             'status' => 1,
