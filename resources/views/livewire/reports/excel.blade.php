0187373010a89ac7baf052b6318f4285970c338f
Janagiramanm <janagiraman@netiapps.com>
2022-03-16 13:15:40 +0530

    debug

---
+++ resources/views/livewire/reports/excel.blade.php
@@ -0,0 +1,26 @@
+<table class="min-w-full leading-normal">
+                             <tr>
+                                <th rowspan="2" class="px-5 py-3 border-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"> 
+                                    S.No  
+                                </th>
+                                <th rowspan="2"  class="px-5 py-3 border-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"> 
+                                    Item 
+                                </th>
+                                <th colspan="2" class="px-5 py-3 border-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-700 uppercase tracking-wider"> 
+                                   Used
+                                  
+                                </th>
+                                <th colspan="2" class="px-5 py-3 border-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-700 uppercase tracking-wider"> 
+                                   Wastage
+                                </th>
+                                
+                               
+                             </tr>
+                               <tr>
+                                    <th class="px-5 py-3 border-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Quantity</th>
+                                    <th class="px-5 py-3 border-2 border-gray-200 bg-gray-100 text-right text-xs font-semibold text-gray-700 uppercase tracking-wider">Price</th>
+                                    <th class="px-5 py-3 border-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Quantity</th>
+                                    <th class="px-5 py-3 border-2 border-gray-200 bg-gray-100 text-right text-xs font-semibold text-gray-700 uppercase tracking-wider">Price</th>
+                                </tr>
+
+</table>
