4525b4e9ded5df8b1b7624b8faa1d5a0ba4a7a3b
Janagiramanm <janagiraman@netiapps.com>
2022-03-16 13:04:05 +0530

    debug

--- app/Http/Livewire/Sales.php
+++ app/Http/Livewire/Sales.php
@@ -13,7 +13,7 @@ class Sales extends Component
     public $show = true;
     public $inputs = [];
     public $i=0;
-    public $months = ['1'=>'January','2'=>'February','2'=>'March','4'=>'April','5'=>'May','6'=>'June','7'=>'July','8'=>'August','9'=>'September','10'=>'October','11'=>'November','12'=>'December'];
+    public $months = ['1'=>'January','2'=>'February','3'=>'March','4'=>'April','5'=>'May','6'=>'June','7'=>'July','8'=>'August','9'=>'September','10'=>'October','11'=>'November','12'=>'December'];
     public $searchTerm = null;
     public $search= null;
     public function render()
